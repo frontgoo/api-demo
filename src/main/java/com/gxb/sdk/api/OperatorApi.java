4e2779ad07cc27cac9466d6eb0005f191a5c5384
yaojun-2 <yaojun@treefinance.com.cn>
2017-07-06 01:13:41 +0800

    update Operator Api

--- src/main/java/com/gxb/sdk/api/OperatorApi.java
+++ src/main/java/com/gxb/sdk/api/OperatorApi.java
@@ -1,23 +1,69 @@
 package com.gxb.sdk.api;
 
+import com.gxb.sdk.parm.GxbResponse;
+import com.gxb.sdk.parm.LoginInfo;
+import com.gxb.sdk.parm.Status;
+import com.gxb.sdk.parm.config.LoginConfig;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
-import retrofit2.http.Query;
+import retrofit2.http.*;
 
 /**
- * 电商API
+ * 运营商API
  */
 public interface OperatorApi {
     /**
-     * 获取页面授权项所需要的配置
+     * 获取手机号对应的运营商配置
      *
-     * @param req
+     * @param token 用户的授权token
+     * @param phone 用户手机号码
      * @return
      */
-//    @POST("/ecommerce/v2/config/taobao/{token}")
-//    Call<TaskCreateRsp> getTaobaoConf(String token);
+    @GET("operator/v2/phone_config/{token}/{phone_number}")
+    Call<GxbResponse<LoginConfig>> getOperatorInitConf(@Path("token") String token, @Path("phone_number") String phone);
 
+    /**
+     * 获取运营商任务的当前状态
+     *
+     * @param token
+     * @return
+     */
+    @GET("operator/v2/get_status/{token}")
+    Call<GxbResponse<Status>> getStatus(@Path("token") String token);
+
+    /**
+     * 刷新运营商登录的登录图片验证码
+     *
+     * @param token
+     * @return
+     */
+    @GET("operator/v2/refresh_verify_code/{token}")
+    Call<GxbResponse<Status>> refreshLoginPicCode(@Path("token") String token);
+
+    /**
+     * 刷新运营商登录的短信验证码
+     *
+     * @param token
+     * @return
+     */
+    @GET("operator/v2/refresh_sms_code/{token}")
+    Call<GxbResponse<Status>> refreshLoginSmsCode(@Path("token") String token);
+
+    /**
+     * 提交运营商登陆
+     *
+     * @param token
+     * @return
+     */
+    @POST("operator/v2/login_submit/{token}")
+    Call<GxbResponse<Status>> submitLogin(@Path("token") String token, @Body LoginInfo loginInfo);
+
+    /**
+     * 提交运营商短信验证码
+     *
+     * @param token
+     * @return
+     */
+    @POST("operator/v2/code_submit/{token}")
+    @FormUrlEncoded
+    Call<GxbResponse<Status>> submitCode(@Path("token") String token, @Field("code") String code);
 }
